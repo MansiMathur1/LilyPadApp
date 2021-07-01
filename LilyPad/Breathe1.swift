1edfc1502d2b99032d61bf8a77f4f7a09f3b6a62
Jessica Orr <jessicalynne429@gmail.com>
2021-07-01 11:59:09 -0400

    app icons + breathing added

---
+++ LilyPad/Breathe1.swift
@@ -0,0 +1,31 @@
+//
+//  Breathe1.swift
+//  LilyPad
+//
+//  Created by  Scholar on 7/1/21.
+//
+
+import UIKit
+
+class Breathe1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named:"lilyPadBackground")!)
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LilyPad/Breathe2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Breathe2ViewController.swift
+//  LilyPad
+//
+//  Created by  Scholar on 7/1/21.
+//
+
+import UIKit
+
+class Breathe2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named:"lilyPadBackground")!)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
